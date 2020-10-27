ddf605855a9c16b8e534a1ea81827d9c9ffccd6c
Aravinth Panch <me@aravinth.info>
2020-10-27 22:34:06 +0100

    Add Advosense CTO job posting

---
+++ advosense/index.php
@@ -0,0 +1,3 @@
+<?php include('meta.php');  ?>
+<?php include('../cookie-consent.php');  ?>
+<embed src="2020-10-27-advosense-cto.pdf" type="application/pdf" height="100%" width="100%" class="responsive">
---
+++ advosense/meta.php
@@ -0,0 +1,23 @@
+<!-- HTML Meta Tags -->
+<title>Advosense | CTO | Berlin</title>
+<meta name="description" content="Advosense | CTO | Berlin">
+
+<!-- Google / Search Engine Tags -->
+<meta itemprop="name"content="Advosense | CTO | Berlin">
+<meta itemprop="description" content="Advosense | CTO | Berlin">
+<meta itemprop="image" content="../images/talent-exposed-banner.png">
+
+<!-- Facebook Meta Tags -->
+<meta property="og:url" content="https://jobs.aravinth.info">
+<meta property="og:type" content="website">
+<meta property="og:title"content="Advosense | CTO | Berlin">
+<meta property="og:description" content="Advosense | CTO | Berlin">
+<meta property="og:image" content="../images/talent-exposed-banner.png">
+
+<!-- Twitter Meta Tags -->
+<meta name="twitter:card" content="summary_large_image">
+<meta name="twitter:title"content="Advosense | CTO | Berlin">
+<meta name="twitter:description" content="Advosense | CTO | Berlin">
+<meta name="twitter:image" content="../images/talent-exposed-banner.png">
+
+<!-- Meta Tags Generated via http://heymeta.com -->
